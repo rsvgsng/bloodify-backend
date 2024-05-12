b7ff83758ae43170bb19fc2d4fd2f1e2f1ef583a
rishav <ghisingrishav@gmail.com>
2024-05-12 18:37:49 +0545

    Added External Modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './Auth/Auth.module';
+import { MainService } from './Main/Main.service';
+import { MainModule } from './Main/Main.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, MainModule],
   controllers: [],
   providers: [],
 })
